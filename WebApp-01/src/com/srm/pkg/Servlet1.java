2e045a14b90e09e22cfae2264ba61650b1099bbb
Sadia.Rashid <Sadia.Rashid@S02605.2d.hq>
2018-04-20 11:18:24 -0400

    1. Made changes to the servlet and index.html to include Ajax echo

--- WebApp-01/src/com/srm/pkg/Servlet1.java
+++ WebApp-01/src/com/srm/pkg/Servlet1.java
@@ -26,8 +26,8 @@ public class Servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
-		out.print("Hello World");
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//out.print("Hello World");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
